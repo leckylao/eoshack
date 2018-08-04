86447e35806b77f753dc2fceb46bc7322538dbfa
ali khorsandian <alikhorsandian@gmail.com>
2018-08-04 17:39:21 +1000

    added labels

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var bioApp = angular.module('bioApp', []);
 
 bioApp.controller('submitCtrl', function ($scope) {
     // init
-    $scope.categories = ['not selected','bird', 'cat', 'dog', 'mouse'];
+    $scope.categories = ['Not selected','bird', 'cat', 'dog', 'mouse'];
 
     $scope.selectedCategory = $scope.categories[0];
     $scope.imageURL = null;

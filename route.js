01cfe774a723d446c787ac75bb137fa7ac59c0e4
ali khorsandian <alikhorsandian@gmail.com>
2018-08-04 19:05:23 +1000

    adding route

---
+++ route.js
@@ -0,0 +1,23 @@
+bioApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/user/requests', {
+            templateUrl: function () {
+                return 'requests.html';
+            }
+        })
+        .when('/user/submit', {
+            templateUrl: function () {
+                return 'submit.html';
+            }
+        })
+        .when('/experts/jobs', {
+            templateUrl: function () {
+                return 'jobs.html';
+            }
+        })
+        .when('/user/verify', {
+            templateUrl: function () {
+                return 'verify.html';
+            }
+        })
+});
